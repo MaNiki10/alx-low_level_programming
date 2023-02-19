c1323edf700c5f8c2958cfbe388ee54344157e1f
Nikita <nikitamampofu@gmail.com>
2023-02-19 15:28:19 -0800

    Task 0-3

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	printf("Size of char : %zu byte(s)\n", sizeof (char));
+	printf("Size of int : %zu byte(s)\n", sizeof (int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof (long int));
+	printf("Size of a float: %zu byte(s)\n", size of (float));
+	 Return (0);
+}	 
