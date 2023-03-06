fcd4f7c39d2d22b0f0319ff2c321b7275e849c59
Nikita <nikitamampofu@gmail.com>
2023-03-06 07:50:52 -0800

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,15 @@
-#include "main.h"                                                                                              
-  2 #include <unistd.h>                                                                                            
-  3                                                                                                                
-  4 /**                                                                                                                                         
-  5  * _putchar - writes the character c to stdout                                                                                              
-  6  * @c: The character to print                                                                                                               
-  7  *                                                                                                                                          
-  8  * Return: On success 1.                                                                                                                    
-  9  * On error, -1 is returned, and errno is set appropriately.                                                                                
- 10  */                                                                                                            
- 11 int _putchar(char c)                                                                                           
- 12 {                                                                                                              
- 13         return (write(1, &c, 1));                                                                              
- 14 }
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
