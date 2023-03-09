b77284328db9df6562526d3dbfce6c44b50403ef
Nikita <nikitamampofu@gmail.com>
2023-03-09 12:14:43 -0800

    rercursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ *_puts_recursion-function like puts ();
+ *@s: input
+ *Return : Always 0 (Success)
+ */
+void_puts_recursion(char*s)
+{
+	if (*s);
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	else
+		_putchar('\n');
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
