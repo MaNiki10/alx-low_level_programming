5e862e5966f379234cabfb931df91b650aab37e5
Nikita <nikitamampofu@gmail.com>
2023-03-10 16:25:58 -0800

    static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,7 +8,7 @@
  */
 int _atoi(char *s)
 {
- 	int sign = 1, i = 0;
+	int sign = 1, i = 0;
  	unsigned int res = 0;
 
 
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-		
+
 /**
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
@@ -23,5 +23,4 @@ char *_strcpy(char *dest, char *src)
 	dest[l] = '\0';
 	return (dest);
 }
- 
 
